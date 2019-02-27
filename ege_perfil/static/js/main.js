5f98d79bf26815d6c174d4561f60f14564c17484
Luiz Antonio <luizvpc@gmail.com>
2019-02-27 17:45:39 -0300

    [UPD] - Edit data

--- ege_perfil/static/js/main.js
+++ ege_perfil/static/js/main.js
@@ -7,37 +7,24 @@ const data = new Vue({
         photo_user: 'https://cdn3.iconfinder.com/data/icons/black-easy/512/538642-user_512x512.png',
         valueDeterminate_1: 100,
         valueDeterminate_2: 30,
-        ege_profile: {
-            username: '',
-
-            is_active: '',
-            is_staff: '',
-            is_superuser: '',
-
-            is_personal_email_public: '',
-
-            biografy: '',
-            is_biografy_public: '',
-
-            valid_photo: '',
-            pending_photo: '',
-            solicitation_at: '',
-            approved_at: '',
-            approved_by: '',
-
-            font_size: '',
-            theme_skin: '',
-            legends: '',
-            sign_language: '',
-            screen_reader: '',
-
-            special_needs: '',
-            is_special_needs_public: ''
-        }
+        dialog_bio: false,
+        dialog_email: false,
+        switch_email: false,
+        switch_needs: true,
+        selected: [],
+        special_needs: [
+            'Autismo',
+            'Doença crônica',
+            'Daltonismo',
+            'Deficiência de aprendizado',
+            'Doença mental',
+            'Deficiência física',
+            'Distúrbios da fala e da linguagem',
+            'Perda auditiva e surdez',
+            'Perda de memória',
+            'Perda de visão e cegueira'
+        ],
     },
-
-    // mounted: function () {
-    //    let ege_profile_json = JSON.parse(document.getElementById('perfil').dataset.content);
-    //    ege_profile_json  = this.ege_profile
-    // },
+    mounted: function () {
+    }
 });
