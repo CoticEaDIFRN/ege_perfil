71dcd16393df44a903dd2e9c9ae5c45914cc591a
Luiz Antonio <luizvpc@gmail.com>
2019-02-28 15:51:01 -0300

    [UPD] - Emails and iterating courses

--- ege_perfil/static/js/main.js
+++ ege_perfil/static/js/main.js
@@ -5,8 +5,6 @@ const data = new Vue({
     data: {
         card_text: 'Lorem ipsum dolor sit amet, brute iriure accusata ne mea. Eos suavitate referrentur ad, te duo agam libris qualisque, utroque quaestio accommodare no qui. Et percipit laboramus usu, no invidunt verterem nominati mel. Dolorem ancillae an mei, ut putant invenire splendide mel, ea nec propriae adipisci. Ignota salutandi accusamus in sed, et per malis fuisset, qui id ludus appareat.',
         photo_user: 'https://cdn3.iconfinder.com/data/icons/black-easy/512/538642-user_512x512.png',
-        valueDeterminate_1: 100,
-        valueDeterminate_2: 30,
         dialog_bio: false,
         dialog_email: false,
         switch_email: false,
@@ -24,7 +22,35 @@ const data = new Vue({
             'Perda de memória',
             'Perda de visão e cegueira'
         ],
+        cursos: [
+            {
+                nome: 'Tecnologia e Desenvolvimento de Software',
+                codigo: '0000.0000.000-00',
+                media: 7.0,
+                aproveitamento: 70,
+                color_bar: ''
+            },
+            {
+                nome: 'Matemática financeira',
+                codigo: '0000.0000.000-00',
+                media: 5.0,
+                aproveitamento: 50,
+                color_bar: ''
+            },
+            {
+                nome: 'Gestão Ambiental',
+                codigo: '0000.0000.000-00',
+                media: null,
+                aproveitamento: 20,
+                color_bar: ''
+            }
+        ],
     },
     mounted: function () {
+        for (let i = 0; i <= this.cursos.length - 1; i++) {
+            if (this.cursos[i].aproveitamento >= 70) { this.cursos[i].color_bar = "success" }
+            else if (this.cursos[i].aproveitamento >= 30 && this.cursos[i].aproveitamento < 70) { this.cursos[i].color_bar = "warning" }
+            else this.cursos[i].color_bar = "error"
+        }
     }
 });
